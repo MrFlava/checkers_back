2014c96a1a29226368552a295748850a86d14053
MrFlava <thatelitemaili33t@gmail.com>
2023-07-24 16:47:05 +0300

    feat: got the MyStatisticsView

--- checkers_statistics/views.py
+++ checkers_statistics/views.py
@@ -1,8 +1,17 @@
-from rest_framework.generics import CreateAPIView
-
+from rest_framework.generics import CreateAPIView, ListAPIView
+from django.db.models import Q
 
 from checkers_statistics.serializers import StatisticsSerializer
+from checkers_statistics.models import Statistics
 
 
 class StatisticsCreateView(CreateAPIView):
     serializer_class = StatisticsSerializer
+
+
+class MyStatisticsView(ListAPIView):
+    serializer_class = StatisticsSerializer
+
+    def get_queryset(self):
+        queryset = Statistics.objects.filter(Q(player_1=self.request.user) | Q(player_2=self.request.user))
+        return queryset
