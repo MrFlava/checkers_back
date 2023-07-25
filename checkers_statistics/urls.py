69bb6e50533738a257f65e2c6834e11fb6a4a47f
MrFlava <thatelitemaili33t@gmail.com>
2023-07-25 15:29:14 +0300

    feat: provide "my-statistics" endpoint

--- checkers_statistics/urls.py
+++ checkers_statistics/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from checkers_statistics.views import StatisticsCreateView
+from checkers_statistics.views import StatisticsCreateView, MyStatisticsView
 
 app_name = "statistics"
 
 urlpatterns = [
     path('create/', StatisticsCreateView.as_view(), name='create-game-statistics'),
+    path('my-statistics/', MyStatisticsView.as_view(), name='my-statistics')
 ]
--- checkers_statistics/views.py
+++ checkers_statistics/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 
+from auth_app.authentication import TokenAuthentication
 from checkers_statistics.serializers import StatisticsSerializer
 from checkers_statistics.models import Statistics
 
@@ -11,6 +13,8 @@ class StatisticsCreateView(CreateAPIView):
 
 class MyStatisticsView(ListAPIView):
     serializer_class = StatisticsSerializer
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication,)
 
     def get_queryset(self):
         queryset = Statistics.objects.filter(Q(player_1=self.request.user) | Q(player_2=self.request.user))
